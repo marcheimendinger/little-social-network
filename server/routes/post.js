d6ec58a64343567c22c281992923026a237feb95
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-04-27 13:02:27 +0200

    Add '/post/feed' function (TODO : add users' names)

--- server/routes/post.js
+++ server/routes/post.js
@@ -17,4 +17,55 @@ router.post('/publish', tools.isAuthenticated, (req, res) => {
     })
 })
 
+// Get latest posts and shares from authenticated user's friends
+router.get('/feed', tools.isAuthenticated, (req, res) => {
+    const connectedUserId = req.user.id
+    const paging = req.body.paging * 10 // [0..n]
+    const query = ` SELECT
+                        DISTINCT posts.post_user_id,
+                        posts.share_user_id,
+                        posts.post_id,
+                        posts.content,
+                        posts.created
+                    FROM
+                    (
+                        SELECT
+                            user_one_id,
+                            user_two_id
+                        FROM friends
+                        WHERE (user_one_id = ? OR user_two_id = ?) AND accepted = true
+                    ) AS userFriends
+                    JOIN
+                    (
+                        SELECT
+                            posts.id AS post_id,
+                            posts.user_id AS post_user_id,
+                            shares.user_id AS share_user_id,
+                            posts.content,
+                            shares.created
+                        FROM shares
+                        LEFT JOIN posts ON shares.post_id = posts.id
+                        UNION
+                        SELECT
+                            id AS post_id,
+                            user_id AS post_user_id,
+                            null AS share_user_id,
+                            content,
+                            created
+                        FROM posts
+                    ) AS posts
+                    WHERE
+                    userFriends.user_one_id = posts.post_user_id OR
+                    userFriends.user_two_id = posts.post_user_id
+                    ORDER BY posts.created DESC
+                    LIMIT 10 OFFSET ?`
+    database.query(query, [connectedUserId, connectedUserId, paging], (err, results) => {
+        if (err) {
+            return res.status(500).send({'error': err})
+        }
+        return res.send(results)
+    })
+
+})
+
 module.exports = router
