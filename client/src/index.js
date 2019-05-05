58f1dc0d7734beebd23c99e17a2c4d5ed0a65514
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-05-05 16:49:16 +0200

    Install Bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
-
+import 'bootstrap/dist/css/bootstrap.css'
 
 import App from './App'
 
