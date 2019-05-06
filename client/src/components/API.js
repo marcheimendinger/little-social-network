37714e5bfdc24298f8a59c09d06fd41360c3e5a5
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-05-06 22:51:16 +0200

    Add date string conversion to date object

--- client/src/components/API.js
+++ client/src/components/API.js
@@ -1,6 +1,18 @@
 import Axios from 'axios'
 
+const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
+
 export default Axios.create({
     baseURL: 'http://localhost:3001/',
-    withCredentials: true
+    withCredentials: true,
+    // Convert date from string to date object
+    // Inspiration : https://mariusschulz.com/blog/deserializing-json-strings-as-javascript-date-objects
+    transformResponse: [(data) => {
+        return JSON.parse(data, (key, value) => {
+            if (typeof value === "string" && dateFormat.test(value)) {
+                return new Date(value)
+            }
+            return value
+        })
+    }]
 })
