a8d30a063a6d35f4fb18dd447290bb597fcb9c13
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-05-07 13:36:55 +0200

    Add publish function to Home

--- client/src/components/restricted/Home.js
+++ client/src/components/restricted/Home.js
@@ -1,16 +1,69 @@
 import React, { useState, useEffect, Fragment } from 'react'
+import { Form, Button, Alert } from 'react-bootstrap'
+import { Formik, Field } from 'formik'
+import * as Yup from 'yup'
 
 import API from '../API'
 
+import Input from '../FormInput'
 import Post from './ui/Post'
 
 export default function Home() {
+    
+    // Publish a post
+
+    const [refresh, setRefresh] = useState(false)
+
+    const validationSchema = Yup.object().shape({
+        post_content: Yup.string()
+            .required()
+            .max(500)
+    })
+
+    async function handleSubmit(values, actions) {
+        try {
+            await API.post('/post/publish', {
+                post_content: values.post_content
+            })
+            actions.resetForm()
+            setRefresh(!refresh)
+        } catch (e) {
+            console.log(e)
+            actions.setSubmitting(false)
+            actions.setStatus({ error: 'Error. Please retry.' })
+        }
+    }
+
+    function Poster() {
+        return (
+            <Formik
+                initialValues={{
+                    post_content: ''
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+                render = {({ handleSubmit, status }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                        { status ? <Alert variant="danger">{status.error}</Alert> : null }
+                        <Field name="post_content" id="post-input" placeholder="What's up ?" component={Input.Textarea} />
+                        <Button type="submit" variant="outline-danger">Post</Button>
+                    </Form>
+                )}
+            />
+        )
+    }
+
+    // Fetch feed
 
     const [feed, setFeed] = useState([])
 
     async function getFeed() {
         try {
-            const results = await API.get('/post/feed')
+            const results = await API.get('/post/feed', {
+                params: {
+                    paging: 0
+                }
+            })
             setFeed(results.data)
         } catch (e) {
             console.log(e)
@@ -27,11 +80,11 @@ export default function Home() {
 
     useEffect(() => {
         getFeed()
-    }, [])
+    }, [refresh]) // Will re-run when `refresh` is updated
 
     return (
         <Fragment>
-            <h1>Home</h1>
+            <Poster />
             <Feed />
         </Fragment>
     )
