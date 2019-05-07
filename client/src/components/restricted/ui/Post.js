b44c762ce8636d3f42e35e9fa653885fcd279a3d
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-05-07 09:56:14 +0200

    Add users informations in Post component

--- client/src/components/restricted/ui/Post.js
+++ client/src/components/restricted/ui/Post.js
@@ -5,18 +5,16 @@ import ReactTimeAgo from 'react-time-ago/commonjs/ReactTimeAgo'
 
 export default function Post(props) {
     return (
-        <Card>
+        <Card className="m-4">
             <Card.Header>
                 <ul className="list-inline mb-0">
                     {props.data.share_user_id ? (
                         <li className="list-inline-item text-info">
-                            {/* <FaRetweet /> {props.data.share_first_name} {props.data.share_last_name} */}
-                            <FaRetweet /> {props.data.share_user_id}
+                            <FaRetweet /> {props.data.share_first_name} {props.data.share_last_name}
                         </li>
                     ) : null}
                     <li className="list-inline-item">
-                        {/* {props.data.post_first_name} {props.data.post_last_name} */}
-                        {props.data.post_user_id}
+                        {props.data.post_first_name} {props.data.post_last_name}
                     </li>
                     <li className="list-inline-item">
                         <small className="text-muted"><ReactTimeAgo date={props.data.created} /></small>
