5d0b8fb2d62486ca2f8b0bdd8eb8a7201a72fec1
marcheimendinger <43568598+mars3110@users.noreply.github.com>
2019-05-18 23:12:52 +0200

    Add Share function

--- client/src/components/restricted/ui/Post.js
+++ client/src/components/restricted/ui/Post.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { FaRetweet } from 'react-icons/fa'
 import ReactTimeAgo from 'react-time-ago/commonjs/ReactTimeAgo'
 
-// Single post view
+import { post } from '../../API'
+
+// Single post view with share button
 // Required props : 'data' (post object from server)
 export default function Post(props) {
+    const [shared, setShared] = useState(false)
+
+    function postShare() {
+        post('/post/share', { post_id: props.data.post_id })
+        setShared(true)
+    }
+
+    function Share() {
+        return (
+            <Button variant="link" className="text-danger p-0" disabled={shared} onClick={postShare}>
+                <FaRetweet className="mb-1" /> Share
+            </Button>
+        )
+    }
+
     return (
         <Card className="my-4">
             <Card.Header>
                 <ul className="list-inline mb-0">
                     {props.data.share_user_id ? (
                         <LinkContainer to={'/user/' + props.data.share_user_id}>
-                            <Button variant="link" className="list-inline-item text-danger p-0 align-top" title={props.data.share_username}>
+                            <Button variant="link" className="list-inline-item text-danger p-0" title={props.data.share_username}>
                                 <FaRetweet /> {props.data.share_first_name} {props.data.share_last_name}
                             </Button>
                         </LinkContainer>
                     ) : null}
                     <LinkContainer to={'/user/' + props.data.post_user_id}>
-                        <Button variant="link" className="list-inline-item text-dark p-0 align-top" title={props.data.post_username}>
+                        <Button variant="link" className="list-inline-item text-dark p-0" title={props.data.post_username}>
                             {props.data.post_first_name} {props.data.post_last_name}
                         </Button>
                     </LinkContainer>
-                    <li className="list-inline-item">
+                    <li className="list-inline-item align-middle">
                         <small className="text-muted"><ReactTimeAgo date={props.data.created} /></small>
+                    </li>
+                    <li className="list-inline-item float-right">
+                        <Share />
                     </li>
                 </ul>
             </Card.Header>
